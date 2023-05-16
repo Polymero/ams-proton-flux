4f4d52b52fe624511a92095d68100838ba3d641d
Sebastiaan Venendaal <svenenda@lxplus744.cern.ch>
2023-05-16 12:44:32 +0200

    20230516

--- HistMaker.sh
+++ HistMaker.sh
@@ -3,4 +3,4 @@
 export ROOTSYS=/cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.20.08/x86_64-centos7-gcc48-opt
 source $ROOTSYS/bin/thisroot.sh
 
-root -b -q /afs/cern.ch/user/s/svenenda/public/test.C
+root -b -q /afs/cern.ch/user/s/svenenda/public/ams-proton-flux/HistMaker.C
