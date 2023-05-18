ee506f4ab41a383312acdd988d554cb10f87acba
Sebastiaan Venendaal <sebastiaanvenendaal@gmail.com>
2023-05-18 16:11:43 +0200

    Update GraphDrawer.C

--- GraphDrawer/GraphDrawer.C
+++ GraphDrawer/GraphDrawer.C
@@ -129,8 +129,6 @@ void LIMI::run() {
 
     // Fill arrays
     for (int i=0; i < binNumber; i++) {
-        
-        cout << hEventsDetected->GetBinContent(i + 1) << endl;
 
         eventsDetected[i] = hEventsDetected->GetBinContent(i + 1);
         eventsSelected[i] = hEventsSelected->GetBinContent(i + 1);
